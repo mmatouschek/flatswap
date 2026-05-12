e4af3c6fa9986dfc032c5800f12b4d0ca92f9d3b
alexeybayev <alexey.baew.1995@gmail.com>
2026-05-12 20:24:53 +0200

    Merged main into alexey-features

--- app/ListSearch.tsx
+++ app/ListSearch.tsx
@@ -1,22 +1,28 @@
-import React, { useState } from "react";
+import { default as React, default as React, useState } from "react";
 import { Alert, View } from "react-native";
 import { filterDate, getResults } from "../backend/FlatSwapAPI";
 import { DefaultStyles } from "../components/ui/DefaultStyles";
 import FlatSwapCalendar from "../components/ui/FlatSwapCalendar";
 import FlatSwapIconButton from "../components/ui/FlatSwapIconButton";
-<<<<<<< HEAD
-import FlatSwapInput from "../components/ui/FlatSwapInput";
-import FlatSwapScrollList from "../components/ui/FlatSwapScrollList";
+import { useSearchData } from "./SearchData";
 
 export default function ListSearch() {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState(useSearchData((state) => state.query));
   const [calendarVisible, setCalendarVisible] = useState(false);
-  const [searchResult, setSearchResult] = useState([]);
-  const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
-  const [current, setCurrent] = useState("start");
+  const [searchResult, setSearchResult] = useState(
+    useSearchData((state) => state.searchResult),
+  );
+  const [startDate, setStartDate] = useState(
+    useSearchData((state) => state.startDate),
+  );
+  const [endDate, setEndDate] = useState(
+    useSearchData((state) => state.endDate),
+  );
+  const [current, setCurrent] = useState(
+    useSearchData((state) => state.current),
+  );
   return (
-    <View style={{ flex: 1, padding: 20 }}>
+    <View style={{ flex: 1, padding: 10 }}>
       <View style={{ flex: 1 }}>
         <View
           style={{
@@ -29,10 +35,14 @@ export default function ListSearch() {
           <FlatSwapInput
             value={query}
             style={[DefaultStyles.input, { flex: 9 }]}
-            onChangeText={setQuery}
+            onChangeText={(val) => {
+              setQuery(val);
+              useSearchData.getState().setQuery(val);
+            }}
             onSubmitEditing={() => {
-              const result = getResults(query);
-              setSearchResult(filterDate(startDate, endDate, result));
+              const result = filterDate(startDate, endDate, getResults(query));
+              setSearchResult(result);
+              useSearchData.getState().setSearchResult(result);
               if (result.length < 1) {
                 const message = `Unfortunately there are no swappers in ${query} in that timeframe!`;
                 Alert.alert("No results", message, [
@@ -52,29 +62,50 @@ export default function ListSearch() {
           startDate={startDate}
           endDate={endDate}
           isVisible={calendarVisible}
-          top={"70"}
-          left="10"
           onBackdropPress={() => setCalendarVisible(false)}
           onDatePress={(day) => {
             if (current == "start") {
               setEndDate(null);
               setStartDate(day.dateString);
               setCurrent("end");
-              const result = getResults(query);
-              setSearchResult(filterDate(day.dateString, endDate, result));
+              const result = filterDate(
+                day.dateString,
+                endDate,
+                getResults(query),
+              );
+              setSearchResult(result);
+
+              useSearchData.getState().setStartDate(day.dateString);
+              useSearchData.getState().setCurrent("end");
+              useSearchData.getState().setEndDate(null);
+              useSearchData.getState().setSearchResult(result);
             } else {
               const start = new Date(startDate);
               const end = new Date(day.dateString);
               if (end.getTime() > start.getTime()) {
                 setEndDate(day.dateString);
                 setCurrent("start");
-                const result = getResults(query);
-                setSearchResult(filterDate(startDate, day.dateString, result));
+                const result = filterDate(
+                  startDate,
+                  day.dateString,
+                  getResults(query),
+                );
+                setSearchResult(result);
+
+                useSearchData.getState().setCurrent("start");
+                useSearchData.getState().setEndDate(day.dateString);
+                useSearchData.getState().setSearchResult(result);
               } else {
                 setStartDate(null);
                 setEndDate(null);
-                setSearchResult(getResults(query));
+                const result = getResults(query);
+                setSearchResult(result);
                 setCurrent("start");
+
+                useSearchData.getState().setStartDate(null);
+                useSearchData.getState().setCurrent("start");
+                useSearchData.getState().setEndDate(null);
+                useSearchData.getState().setSearchResult(result);
               }
             }
           }}
@@ -83,104 +114,6 @@ export default function ListSearch() {
       <View style={{ flex: 15 }}>
         <FlatSwapScrollList searchResult={searchResult} />
       </View>
-=======
-import { DefaultStyles } from "../components/ui/DefaultStyles";
-import React, { useState } from "react";
-import { useRouter, Slot, Redirect  } from "expo-router";
-import MapView, { Marker, Circle } from "react-native-maps";
-import { Alert } from "react-native";
-import getAllUsers, {getUser, getResults, getApartmentPictures, filterDate} from "../backend/FlatSwapAPI"
-import { Calendar } from 'react-native-calendars';
-import {useSearchData} from "./SearchData"
-
-
-
-
-
-
-export default function ListSearch() {
-const [query, setQuery] = useState(useSearchData(state => state.query));
-const [calendarVisible, setCalendarVisible] = useState(false);
-const [searchResult, setSearchResult] = useState(useSearchData(state => state.searchResult));
-const [startDate, setStartDate] = useState(useSearchData(state => state.startDate));
-const [endDate, setEndDate] = useState(useSearchData(state => state.endDate));
-const [current, setCurrent] = useState(useSearchData(state => state.current));
-  return (
-		<View style={{ flex: 1, padding: 10}}>
-		<View style = { {flex:1}}>
-		<View style={{flex:1,  gap:5, flexDirection: "row", justifyContent:"center"}}>
-		<FlatSwapInput
-		value={query}
-		style={[DefaultStyles.input,{flex:9}]}
-		onChangeText={(val)=>{
-		setQuery(val);
-		useSearchData.getState().setQuery(val);
-		}}
-		onSubmitEditing={()=>{
-			const result = filterDate(startDate, endDate,getResults(query));
-			setSearchResult(result);
-			useSearchData.getState().setSearchResult(result);
-			if (result.length<1){
-				const message = `Unfortunately there are no swappers in ${query} in that timeframe!`;
-				Alert.alert("No results", message,
-				[{ text: "OK", onPress: () => {} }]);
-			
-			}
-		}}
-		placeholder="..."/>
-		<FlatSwapIconButton style={[DefaultStyles.button,{flex:1}]} icon ="calendar" 
-		onPress = {()=>setCalendarVisible(true)}		/>
-		</View>
-		<FlatSwapCalendar startDate={startDate} endDate={endDate} isVisible={calendarVisible} onBackdropPress = {()=>setCalendarVisible(false)}
-		onDatePress = {
-			(day)=>{
-				if (current=="start"){
-					setEndDate(null)
-					setStartDate(day.dateString);
-					setCurrent("end");
-					const result = filterDate(day.dateString, endDate,getResults(query));
-					setSearchResult(result);
-					
-					useSearchData.getState().setStartDate(day.dateString);
-					useSearchData.getState().setCurrent("end");
-					useSearchData.getState().setEndDate(null);
-					useSearchData.getState().setSearchResult(result);
-				}
-				else{
-
-					const start = new Date(startDate);
-					const end = new Date(day.dateString);
-					if(end.getTime() > start.getTime()){
-					setEndDate(day.dateString);
-					setCurrent("start");
-					const result = filterDate(startDate, day.dateString,getResults(query));
-					setSearchResult( result);
-					
-					useSearchData.getState().setCurrent("start");
-					useSearchData.getState().setEndDate(day.dateString);
-					useSearchData.getState().setSearchResult(result);
-					}
-					else{
-						setStartDate(null);
-						setEndDate(null);
-						const result = getResults(query);
-						setSearchResult(result);
-						setCurrent("start");
-						
-						useSearchData.getState().setStartDate(null);
-						useSearchData.getState().setCurrent("start");
-						useSearchData.getState().setEndDate(null);
-						useSearchData.getState().setSearchResult(result);
-					}
-				}
-				
-		}}
-		/>
-		</View>
-		<View style={{flex: 15}}>
-		<FlatSwapScrollList searchResult={searchResult}/>
-		</View>
->>>>>>> main
     </View>
   );
 }
--- app/MapSearch.tsx
+++ app/MapSearch.tsx
@@ -6,20 +6,35 @@ import FlatSwapCalendar from "../components/ui/FlatSwapCalendar";
 import FlatSwapIconButton from "../components/ui/FlatSwapIconButton";
 import FlatSwapInput from "../components/ui/FlatSwapInput";
 import FlatSwapMap from "../components/ui/FlatSwapMap";
-<<<<<<< HEAD
+import { useSearchData } from "./SearchData";
 
 export default function MapSearch() {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState(useSearchData((state) => state.query));
   const [calendarVisible, setCalendarVisible] = useState(false);
-  const [searchResult, setSearchResult] = useState([]);
-  const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
-  const [current, setCurrent] = useState("start");
-  const [lat, setLat] = useState(40);
-  const [long, setLong] = useState(10);
+  const [searchResult, setSearchResult] = useState(
+    useSearchData((state) => state.searchResult),
+  );
+  const [startDate, setStartDate] = useState(
+    useSearchData((state) => state.startDate),
+  );
+  const [endDate, setEndDate] = useState(
+    useSearchData((state) => state.endDate),
+  );
+  const [current, setCurrent] = useState(
+    useSearchData((state) => state.current),
+  );
+  let initLat = 40;
+  let initLong = 15;
+  if (searchResult.length > 0) {
+    initLat = searchResult[0].latitude;
+    initLong = searchResult[0].longitude;
+  }
+
+  const [lat, setLat] = useState(initLat);
+  const [long, setLong] = useState(initLong);
 
   return (
-    <View style={{ flex: 1, padding: 20 }}>
+    <View style={{ flex: 1, padding: 10 }}>
       <View
         style={{
           flex: 1,
@@ -31,10 +46,14 @@ export default function MapSearch() {
         <FlatSwapInput
           style={[DefaultStyles.input, { flex: 9 }]}
           value={query}
-          onChangeText={setQuery}
+          onChangeText={(val) => {
+            setQuery(val);
+            useSearchData.getState().setQuery(val);
+          }}
           onSubmitEditing={() => {
-            const result = getResults(query);
+            const result = filterDate(startDate, endDate, getResults(query));
             setSearchResult(result);
+            useSearchData.getState().setSearchResult(result);
             if (result.length > 0) {
               setLat(result[0].latitude);
               setLong(result[0].longitude);
@@ -58,29 +77,50 @@ export default function MapSearch() {
         startDate={startDate}
         endDate={endDate}
         isVisible={calendarVisible}
-        top={"70"}
-        left="10"
         onBackdropPress={() => setCalendarVisible(false)}
         onDatePress={(day) => {
           if (current == "start") {
             setEndDate(null);
             setStartDate(day.dateString);
             setCurrent("end");
-            const result = getResults(query);
-            setSearchResult(filterDate(day.dateString, endDate, result));
+            const result = filterDate(
+              day.dateString,
+              endDate,
+              getResults(query),
+            );
+            setSearchResult(result);
+
+            useSearchData.getState().setStartDate(day.dateString);
+            useSearchData.getState().setCurrent("end");
+            useSearchData.getState().setEndDate(null);
+            useSearchData.getState().setSearchResult(result);
           } else {
             const start = new Date(startDate);
             const end = new Date(day.dateString);
             if (end.getTime() > start.getTime()) {
               setEndDate(day.dateString);
               setCurrent("start");
-              const result = getResults(query);
-              setSearchResult(filterDate(startDate, day.dateString, result));
+              const result = filterDate(
+                startDate,
+                day.dateString,
+                getResults(query),
+              );
+              setSearchResult(result);
+
+              useSearchData.getState().setCurrent("start");
+              useSearchData.getState().setEndDate(day.dateString);
+              useSearchData.getState().setSearchResult(result);
             } else {
               setStartDate(null);
               setEndDate(null);
-              setSearchResult(getResults(query));
+              const result = getResults(query);
+              setSearchResult(result);
               setCurrent("start");
+
+              useSearchData.getState().setStartDate(null);
+              useSearchData.getState().setCurrent("start");
+              useSearchData.getState().setEndDate(null);
+              useSearchData.getState().setSearchResult(result);
             }
           }
         }}
@@ -93,129 +133,6 @@ export default function MapSearch() {
           searchResult={searchResult}
         />
       </View>
-=======
-import FlatSwapIconButton from "../components/ui/FlatSwapIconButton";
-import FlatSwapCalendar from "../components/ui/FlatSwapCalendar";
-import { DefaultStyles } from "../components/ui/DefaultStyles";
-import { useState } from "react";
-import { useRouter  } from "expo-router";
-import MapView, { Marker, Circle } from "react-native-maps";
-import { Alert } from "react-native";
-import getAllUsers, {getUser, getResults, getApartmentPictures, filterDate} from "../backend/FlatSwapAPI"
-import { Calendar } from 'react-native-calendars';
-import {useSearchData} from "./SearchData"
-
-export default function MapSearch() {
-const [query, setQuery] = useState(useSearchData(state => state.query));
-const [calendarVisible, setCalendarVisible] = useState(false);
-const [searchResult, setSearchResult] = useState(useSearchData(state => state.searchResult));
-const [startDate, setStartDate] = useState(useSearchData(state => state.startDate));
-const [endDate, setEndDate] = useState(useSearchData(state => state.endDate));
-const [current, setCurrent] = useState(useSearchData(state => state.current));
-let initLat = 40;
-let initLong = 15;
-if (searchResult.length>0){
-	initLat = searchResult[0].latitude;
-	initLong = searchResult[0].longitude;
-}
-
-
-
-const [lat,setLat]= useState(initLat);
-const [long,setLong]=useState(initLong);
-
- 
-  
-  
-  
-  return (
-		
-		<View style={{ flex: 1, padding: 10}}>
-		<View style = {{flex:1, gap:5, flexDirection: "row", justifyContent:"center"}}>
-		<FlatSwapInput
-		style={[DefaultStyles.input,{flex:9}]}
-		value={query}
-		onChangeText={(val)=>{
-		setQuery(val);
-		useSearchData.getState().setQuery(val);
-		}}
-		onSubmitEditing={()=>{
-			const result = filterDate(startDate, endDate,getResults(query));
-			setSearchResult(result);
-			useSearchData.getState().setSearchResult(result);
-			if (result.length>0){
-				setLat(result[0].latitude);
-				setLong(result[0].longitude);
-			}
-			else{
-				const message = `Unfortunately there are no swappers in ${query} at that date!`;
-				Alert.alert("No results", message,
-				[{ text: "OK", onPress: () => {} }]);
-
-			
-			}
-		}}
-		placeholder="..."/>
-		
-		<FlatSwapIconButton style={[DefaultStyles.button,{flex:1}]} icon ="calendar" 
-		onPress = {()=>setCalendarVisible(true)}		/>
-		</View>
-		<FlatSwapCalendar startDate={startDate} endDate={endDate} isVisible={calendarVisible} onBackdropPress = {()=>setCalendarVisible(false)}
-		onDatePress = {
-			(day)=>{
-				if (current=="start"){
-					setEndDate(null)
-					setStartDate(day.dateString);
-					setCurrent("end");
-					const result = filterDate(day.dateString, endDate,getResults(query));
-					setSearchResult(result);
-					
-					useSearchData.getState().setStartDate(day.dateString);
-					useSearchData.getState().setCurrent("end");
-					useSearchData.getState().setEndDate(null);
-					useSearchData.getState().setSearchResult(result);
-					
-				}
-				else{
-
-					const start = new Date(startDate);
-					const end = new Date(day.dateString);
-					if(end.getTime() > start.getTime()){
-					setEndDate(day.dateString);
-					setCurrent("start");
-					const result = filterDate(startDate, day.dateString,getResults(query));
-					setSearchResult( result);
-					
-					useSearchData.getState().setCurrent("start");
-					useSearchData.getState().setEndDate(day.dateString);
-					useSearchData.getState().setSearchResult(result);
-					
-					
-					
-					}
-					else{
-						setStartDate(null);
-						setEndDate(null);
-						const result = getResults(query);
-						setSearchResult(result);
-						setCurrent("start");
-				
-						useSearchData.getState().setStartDate(null);
-						useSearchData.getState().setCurrent("start");
-						useSearchData.getState().setEndDate(null);
-						useSearchData.getState().setSearchResult(result);
-					}
-				}
-				
-		}}
-		/>
-		
-		
-		
-		<View style={{flex: 15, padding: 5}}>
-		<FlatSwapMap latitude={lat} longitude={long} searchResult={searchResult} />
-		</View>		
->>>>>>> main
     </View>
   );
 }
